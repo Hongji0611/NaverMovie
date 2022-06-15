3523800a77bfb637cf41549cc209504123514761
Jiwoo Hong <hjw6112@gmail.com>
2022-06-16 01:42:15 +0900

    fix: HomeStyle

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,3 +10,5 @@ urlpatterns = [
     path('detail/<int:id>', views.movie_detail, name='detail'),
     path('comments/<int:id>', views.comments, name='comments'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
